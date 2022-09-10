162a3096f2208942a195dd1b1abef3b29287df50
nikhiljanshali <nikhil.janshali@gmail.com>
2022-09-10 14:18:49 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import './style.css';
+// import { of } from 'rxjs';
+// import { tap, map, mergeMap, toArray } from 'rxjs/operators';
 
-import { of, map, Observable } from 'rxjs';
+import { of, map, Observable, tap, mergeMap, toArray } from 'rxjs';
 
 of('Nikhil')
   .pipe(map((name) => `Hello, ${name}!`))
@@ -33,3 +35,57 @@ function logItem(val: any) {
   document.getElementById('list').appendChild(node);
 }
 // Open the console in the bottom right to see results.
+
+/**
+ * Test 2
+ */
+
+// const ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const transformTo = (id: number) =>
+//   of(id).pipe(
+//     map((data) => data * 10),
+//     tap({
+//       complete: () => console.log(`Inner value ${id}`),
+//     })
+//   );
+// of(ids)
+//   .pipe(
+//     mergeMap((ids) => ids),
+//     mergeMap((id) => transformTo(id)),
+//     tap({
+//       error: (error) => console.log(`Error with ${ids}: ` + error),
+//     }),
+//     toArray()
+//   )
+//   .subscribe((e) => console.log(e));
+
+console.log('===========================================');
+of('Lorem', 'Ipsum', 'Vice')
+  .pipe(
+    tap((item) => {
+      console.log('Before ' + item);
+    }),
+    map((item) => item.length),
+    tap((item) => {
+      console.log(item);
+      console.log('Before ' + item);
+    }),
+    map((item) => {
+      if (item == 4) {
+        throw Error;
+      }
+      return item;
+    }),
+    tap(
+      (item) => {
+        console.log('After ' + item);
+      },
+      (err) => {
+        console.log('Error: ' + err);
+      },
+      () => {
+        console.log('Complete');
+      }
+    )
+  )
+  .subscribe((item) => console.log(item));
