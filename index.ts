c73ac94687d68dad6a7438a1c6b3e4a7d690bec9
nikhiljanshali <nikhil.janshali@gmail.com>
2022-09-10 15:10:42 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import './style.css';
 // import { of } from 'rxjs';
 // import { tap, map, mergeMap, toArray } from 'rxjs/operators';
 
-import { of, map, Observable, tap, mergeMap, toArray } from 'rxjs';
+import { of, map, Observable, tap, mergeMap, toArray, interval } from 'rxjs';
 
 of('Nikhil')
   .pipe(map((name) => `Hello, ${name}!`))
@@ -40,26 +40,29 @@ function logItem(val: any) {
  * Test 2
  */
 
-// const ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const transformTo = (id: number) =>
-//   of(id).pipe(
-//     map((data) => data * 10),
-//     tap({
-//       complete: () => console.log(`Inner value ${id}`),
-//     })
-//   );
-// of(ids)
-//   .pipe(
-//     mergeMap((ids) => ids),
-//     mergeMap((id) => transformTo(id)),
-//     tap({
-//       error: (error) => console.log(`Error with ${ids}: ` + error),
-//     }),
-//     toArray()
-//   )
-//   .subscribe((e) => console.log(e));
+const ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const transformTo = (id: number) =>
+  of(id).pipe(
+    map((data) => data * 10),
+    tap({
+      complete: () => console.log(`Inner value ${id}`),
+    })
+  );
+of(ids)
+  .pipe(
+    mergeMap((ids) => ids),
+    mergeMap((id) => transformTo(id)),
+    tap({
+      error: (error) => console.log(`Error with ${ids}: ` + error),
+    }),
+    toArray()
+  )
+  .subscribe((e) => console.log(e));
 
 console.log('===========================================');
+/**
+ * Test 3
+ */
 of('Lorem', 'Ipsum', 'Vice')
   .pipe(
     tap((item) => {
@@ -89,3 +92,14 @@ of('Lorem', 'Ipsum', 'Vice')
     )
   )
   .subscribe((item) => console.log(item));
+
+console.log('=============================');
+/**
+ * Test 4
+ */
+
+const r$ = of(4);
+const x$ = of('X');
+// interval(1000)
+//   .pipe(mergeMap((v) => iif(() => v % 4 === 0, r$, x$)))
+//   .subscribe(console.log);
